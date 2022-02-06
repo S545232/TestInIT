c2bdec465908f1200831f3f0b764d392550435d5
ANIL KOLLA <89354787+S545232@users.noreply.github.com>
2022-02-06 13:20:38 -0600

    conflicts resolved

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 # TestInIT
 test
+=======
+# TestInIT
+>>>>>>> edb8ecc (second commit)
---
+++ src/main/java/com/nwmissouri/testinit/App.java
@@ -0,0 +1,8 @@
+package com.nwmissouri.testinit;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+    
+}
